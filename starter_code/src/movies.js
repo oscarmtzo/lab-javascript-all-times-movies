303f5a0529aa38aea948c4b3b1098a661acf824f
oscarmtzo <icehman@hotmail.com>
2021-04-12 15:51:08 -0500

    done till bonus It-8

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -1,169 +1,129 @@
-/* eslint no-restricted-globals: 'off' */
-// Turn duration of the movies from hours to minutes 
-const turnHoursToMinutes = (arr) => {
-    let newArr = []
-    let minutes
-    let str
-    for(let i = 0; i<arr.length; i++){
-      newArr.push(arr[i])
+// Iteration 1: All directors? - Get the array of all directors.
+function turnHoursToMinutes(arr){
+    // console.log(arr) 
+    function minutesHour(hoursAndMinutes){
+        //console.log(hoursAndMinutes) //--> Ej. "1h 18min"
+        minutesOnly = hoursAndMinutes.replace(/h|min/g,'').split(' ') 
+        // console.log(minutesHour)
+        return minutesOnly.reduce((acumulador, currentV)=>{
+            return (Number(acumulador)*60)+Number(currentV)
+        })
     }
-    //console.log('1 auqi')
-    for(let i =0; i<newArr.length; i++){
-      str = newArr[i].duration
-      minutes = parseInt(str.slice(0,1))*60+parseInt(str.slice(2,5))
-     
-      //console.log(minutes)
-      
-    }
-    //console.log('2 auqi')
-    for(let i=0; i<newArr.length; i++){
-      newArr[i].duration = minutes
-    }
-    //console.log('ya no debio pasar nada')
-    console.log(newArr, arr)
-    
-    return newArr
-    //console.log(newArr)
-  }
-
-
-// Get the average of all rates with 2 decimals 
-const ratesAverage = (arr) => {
-    let numRate
-    let suma = 0
-    for(let i = 0; i<arr.length; i++){
-        numRate = arr[i].rate
-        numRate = parseFloat(numRate).toFixed(2)
-        numRate = parseFloat(numRate)
-        suma += numRate
-    }
-    suma = suma/arr.length
-    //console.log(suma)
-    return suma
-  }
-
-// Get the average of Drama Movies
-const dramaMoviesRate = ( arr ) => {
-    let numRate 
-    let suma =0
-    let numeroElementos = 0
-    for(let i = 0; i<arr.length; i++){
-        if(arr[i].genre.includes('Drama')){
-            if(arr[i].rate === undefined || null || 0){
-            numRate = 0
-        }else{
-            numRate = arr[i].rate
-            numeroElementos ++
+    newArr = arr.map((movies)=>{
+        obj = {
+            director: movies.director,
+            duration: Number(minutesHour(movies.duration)),
+            genre: movies.genre,
+            rate: movies.rate,
+            title: movies.title,
+            year: movies.year
         }
-        numRate = parseFloat(numRate).toFixed(2)
-        numRate = parseFloat(numRate)
-        suma += numRate 
-        }
-    }
-    if(suma == 0){
-        suma = undefined
-    }else{
-        suma = suma/numeroElementos 
-        suma = suma.toFixed(2)
-        suma = parseFloat(suma) 
-    }
-    //console.log(suma)
-    return suma
-}
-
-// Order by time duration, in growing order
-const orderByDuration = ( arr ) => {
-    let newArr = []
-    for(let i = 0; i<arr.length; i++){
-        newArr.push(arr[i])
-    }
-    newArr.sort((a,b) => {
-        if (a.duration > b.duration) {
-          return 1;
-        }
-        if (a.duration < b.duration) {
-          return -1;
-        }
-        else if(a.duration === b.duration){
-            newArr.sort((a,b) => {
-                if(a.title > b.title) {
-                    return 1
-                }
-                else if(a.title < b.title){
-                    return -1
-                }
-                else{
-                    return 0
-                }
-            })
-            
-        }
-        else{
-            return 0
-        }
-    });
-    return newArr
-}
-
-// How many movies did STEVEN SPIELBERG
-const howManyMovies = ( arr ) => {
-    let countOfMovies = 0
-    let statementOfMovies
-    if(arr.length ==0){
-        return undefined
-    }else{
-        for(let i = 0; i<arr.length; i++){
-            if(arr[i].director === 'Steven Spielberg' && arr[i].genre.includes('Drama')){
-                countOfMovies ++
-                statementOfMovies = `Steven Spielberg directed ${countOfMovies} drama movies!`
-            }else {
-                statementOfMovies = `Steven Spielberg directed ${countOfMovies} drama movies!`
-            }
-        }
-        countOfMovies.toString()
-        return statementOfMovies
-    }
-    
-}
-
-// Order by title and print the first 20 titles
-const orderAlphabetically = ( arr ) => {
-    let newArr = []
-    let lengthArr = arr.length
-    for(let i = 0; i<lengthArr; i++){
-        if(arr.length>20){
-            lengthArr = 20
-        }
-        newArr.push(arr[i].title)
-    }
-    newArr.sort( (a,b) => {
-        if ( a > b ){
-            return 1
-        }else if ( a < b ){
-            return -1
-        }else {
-            return 0
-        }
-
+        return obj
     })
     return newArr
 }
+// _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors. How could you "clean" a bit this array and make it unified (without duplicates)?
 
-// Best yearly rate average
-const bestYearAvg = ( arr ) => {
-    let newArr = []
-    let sum = 0
-    let obj = {}
-    if( arr.length == 0){
+// Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
+
+// Iteration 3: All rates average - Get the average of all rates with 2 decimals
+
+function ratesAverage(arrMovies){
+    //console.log(arrMovies)
+    rate = arrMovies.reduce((sum, currentV)=>{
+        
+        return sum + Number(currentV.rate)},0)
+    rate/=arrMovies.length 
+    rate= Number(rate.toFixed(2))
+    return rate
+}
+// Iteration 4: Drama movies - Get the average of Drama Movies
+function dramaMoviesRate(arrMovies){
+    arrDramaMovies = arrMovies.filter((movies)=>{
+        return movies.genre.includes('Drama')
+    })
+    if(arrDramaMovies.length>0){
+        return ratesAverage(arrDramaMovies)
+    }else {
+        return undefined
+    }
+}
+// Iteration 5: Ordering by year - Order by year, ascending (in growing order)
+
+// Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
+function orderAlphabetically (arrMovies){
+    if(arrMovies.length===0){
+        return []
+    }
+    if(arrMovies.length<2){
+        return arrMovies[0].title
+    }
+    
+    ordered = arrMovies.sort((a,b)=>{
+        if(a.title>b.title){return 1}
+        if(a.title<b.title){return -1}
+        return 0
+    })
+    if(arrMovies.length<20){
+        onlyTitleArr= ordered.map((e)=>{
+            return e.title})
+        return onlyTitleArr
+    }
+    orderedTwenty = [] //length = 0
+
+    for(let i=0; i<20; i++){
+        orderedTwenty.push(ordered[i].title)
+    }
+    // console.log(orderedTwenty.length) //length = 20
+    // console.log(orderedTwenty[0].title)
+    console.log(orderedTwenty,'orderedTwenty')
+    return orderedTwenty
+}
+// BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
+
+// BONUS - Iteration 8: Best yearly rate average - Best yearly rate average
+function bestYearAvg(arrMovies){
+    if(arrMovies.length===0){
         return undefined
     }else{
-        for(let i = 0; i<arr.length; i++){
-
-            if(newArr.includes(arr[i].year)==false){
-                newArr.push(arr[i].year)
+        
+        arrMoviesYearly =arrMovies.map((movie)=>{
+            return {
+                year: movie.year,
+                rate: movie.rate
             }
-        }
-        return sum
-    }
+        })
 
+    }
+}
+
+// function createObjYearly (year,rate){
+//     let objYear
+//     objYear[year] = {
+//         rates: []
+//     }
+//     // objYear[year].rates.push(rate)
+//     return objYear
+// }
+function orderByDuration(arrMovies){
+    console.log(arrMovies)
+    arrOrdered = arrMovies.sort((a,b)=>{
+        if(a.duration == b.duration){
+            return -1
+        }else {
+            return a.duration - b.duration
+        }
+    })
+    console.log(arrOrdered)
+    return arrOrdered
+}
+
+function howManyMovies(arrMovies){
+    if(arrMovies.length===0){
+        return undefined
+    }
+    arrSteven = arrMovies.filter((element)=>{
+        return element.director==='Steven Spielberg'&&element.genre.includes('Drama')
+    })
+    return `Steven Spielberg directed ${arrSteven.length} drama movies!`
 }
